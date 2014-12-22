d393c17881a17c812e7105286586ead3a2b122bd
hww712 <skymatrix@126.com>
2014-12-22 22:30:41 +0800

    finished thubmail

--- haveku/vedios/parsevedio.py
+++ haveku/vedios/parsevedio.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 
 
+import re
+
 def get_vid_of_url(url):
-    pass
+    re_of_youku = re.compile("(?<=/v_show/id_).*(?=\.html)", re.IGNORECASE)
+    re_of_tudou = re.compile("(?<=/programs/view/).*(?=/)", re.IGNORECASE)
+    re_of_56com = re.compile("(?<=/v_).*(?=\.html)", re.IGNORECASE)
+    
+    if "youku.com" in url:
+        return re_of_youku.findall(url)
+    elif "tudou.com" in url:
+        return re_of_tudou.findall(url)
+    elif "56.com" in url:
+        return re_of_56com.findall(url)
+    else:
+        return ""
+
+
 
 
 def parse_url(url):
@@ -12,7 +27,24 @@ def parse_url(url):
 
 
 
-
+if __name__ == "__main__":
+    url = "http://v.youku.com/v_show/id_XODU0MzA4NzI0.html?ev=5&from=y1.1-2.10001-0.1-2"
+    re_of_youku = re.compile("(?<=/v_show/id_).*(?=\.html)", re.IGNORECASE)
+    vid = re_of_youku.findall(url)
+    print(vid)
+    
+    url = "http://www.tudou.com/programs/view/YzIFnviBarw/?fr=rec2"
+    re_of_tudou = re.compile("(?<=/programs/view/).*(?=/)", re.IGNORECASE)
+    vid = re_of_tudou.findall(url)
+    print(vid)
+    
+    url = "http://www.56.com/u63/v_MTI4OTgxNjUy.html"
+    re_of_56com = re.compile("(?<=/v_).*(?=\.html)", re.IGNORECASE)
+    vid = re_of_56com.findall(url)
+    print(vid)
+    
+    print(get_vid_of_url(url))
+    
 
 
 
--- haveku/vedios/thumbnail.py
+++ haveku/vedios/thumbnail.py
@@ -6,88 +6,57 @@ import json, hashlib, time
 from urllib import request
 
 
-def preview_youku(vid):
-    url = r'http://v.youku.com/player/getPlayList/VideoIDS/%s' % vid
-    
-    '''
-    proxy = urllib2.ProxyHandler({'http': r'http://SCHENKER-ASIA\name:pwd@sscproxy.ap.signintra.com:80'})
-    opener = urllib2.build_opener(proxy)
-    urllib2.install_opener(opener)
-    '''
-    
-    readstring = request.urlopen(url).read()
-    
-    
-    dic = json.loads(readstring.decode('utf-8'))
-    
-    if 'data' in dic.keys():
-        if 'logo' in dic['data'][0].keys():
-            return dic['data'][0]['logo']
-    
-    return ""
-    
-    
-    '''
-    match = re.search('"logo":"[^"]*"', readstring)
-    
-    if match:
-        image_url = unicode(m.group().split(":", 1)[1][1:-1]).replace(r"\/", r"/")
-        return image_url
+def parse_json_from_web_interface(domain, vid):
+    if domain.lower() == "youku.com":
+        url = r'http://v.youku.com/player/getPlayList/VideoIDS/%s' % vid
+    elif domain.lower() == "tudou.com":
+        url = r'http://api.tudou.com/v6/video/info?app_key=225d19f553bde37e&format=json&itemCodes=%s' % vid
+    elif domain.lower() == "56.com":
+        ts = str(int(time.time()))
+        
+        appkey = "3000004324"
+        appsecret = "3b361ba0d77e7311"
+        
+        m = hashlib.md5()
+        s = "vid=%s" % vid
+        m.update(s.encode(encoding='utf_8', errors='strict'))
+        sign1 = m.hexdigest()
+        
+        m = hashlib.md5()
+        s = "%s#%s#%s#%s" %(sign1, appkey, appsecret, ts)
+        m.update(s.encode(encoding='utf_8', errors='strict'))
+        sign = m.hexdigest()
+        
+        url = r'http://oapi.56.com/video/getVideoInfo.json?appkey=%s&sign=%s&ts=%s&vid=%s' % (appkey, sign, ts, vid)
     else:
-        return 'No match'
-    '''
+        return "ERROR!"
     
-
-def preview_tudou(vid):
-    url = r'http://api.tudou.com/v6/video/info?app_key=225d19f553bde37e&format=json&itemCodes=%s' % vid
     
     readstring = request.urlopen(url).read()
     
-    rst = json.loads(readstring.decode('utf-8'))
-    
-    if 'results' in rst.keys():
-        if 'bigPicUrl' in rst['results'][0].keys():
-            return rst['results'][0]['bigPicUrl']
-    
-    return ""
+    return json.loads(readstring.decode('utf-8'))
 
 
-def previe_56(vid):
-    '''
-    http://dev.56.com/wiki/index.php?doc-view-20
-    '''
-    ts = str(int(time.time()))
+def get_thumbnail_from_web_interface(domain, vid):
     
-    appkey = "3000004324"
-    appsecret = "3b361ba0d77e7311"
+    json_dict = parse_json_from_web_interface(domain, vid)
     
-    m = hashlib.md5()
-    #m.update("vid=%s" % vid)
-    a = "vid=%s" % vid
-    m.update(a.encode(encoding='utf_8', errors='strict'))
-    sign1 = m.hexdigest()
+    if domain.lower() == "youku.com":
+        if 'data' in json_dict.keys():
+            if 'logo' in json_dict['data'][0].keys():
+                return json_dict['data'][0]['logo']
     
-    m = hashlib.md5()
-    a = "%s#%s#%s#%s" %(sign1, appkey, appsecret, ts)
-    m.update(a.encode(encoding='utf_8', errors='strict'))
-    sign = m.hexdigest()
+    if domain.lower() == "tudou.com":
+        if 'results' in json_dict.keys():
+            if 'bigPicUrl' in json_dict['results'][0].keys():
+                return json_dict['results'][0]['bigPicUrl']
     
-    '''
-    proxy = urllib2.ProxyHandler({'http': r'http://SCHENKER-ASIA\name:pwd@sscproxy.ap.signintra.com:80'})
-    opener = urllib2.build_opener(proxy)
-    urllib2.install_opener(opener)
-    '''
+    if domain.lower() == "56.com":
+        if '0' in json_dict.keys():
+            if 'mimg' in json_dict['0'].keys():
+                return json_dict['0']['mimg']
     
-    url = r'http://oapi.56.com/video/getVideoInfo.json?appkey=%s&sign=%s&ts=%s&vid=%s' % (appkey, sign, ts, vid)
-    readstring = request.urlopen(url).read()
-    
-    dic = json.loads(readstring.decode('utf-8'))
-    
-    if '0' in dic.keys():
-        if 'mimg' in dic['0'].keys():
-            return dic['0']['mimg']
-    
-    return ""
+    return "ERROR!"
 
 
 
@@ -95,10 +64,14 @@ def previe_56(vid):
 
 
 if __name__ == "__main__":
-#     print(preview_youku('XMzM5NTU2MDcy'))
-#     print(preview_tudou('f6I9_UWl5Yo'))
-
-    print(previe_56('MTExODU1NTI5'))
+    
+    print(get_thumbnail_from_web_interface("youku.com", 'XMzM5NTU2MDcy'))
+    print(get_thumbnail_from_web_interface("tudou.com", 'f6I9_UWl5Yo'))
+    print(get_thumbnail_from_web_interface("56.com", 'MTExODU1NTI5'))
+    
+    
+    
+    
 
 
 
